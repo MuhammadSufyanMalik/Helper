d295ec3db9acd9b3d446131829334f0305243717
Muhammad Sufyan MALIK <muhammad.sufyan@ardgrup.com.tr>
2023-07-19 17:34:34 +0300

    Add project files.

---
+++ FileHelpers.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Results;
+
+
+namespace Helper
+{
+    public class FileHelpers
+    {
+        public static string Add(IFormFile file)
+        {
+            var sourcePath = Path.GetTempFileName();
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream(sourcePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            var result = newPath(file);
+            File.Move(sourcePath, result);
+
+            return result;
+        }
+
+        public static IResult Delete(string path)
+        {
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception exeption)
+            {
+
+                return new ErrorResult(exeption.Message);
+            }
+            return new SuccessResult();
+        }
+
+        public static string Update(string sourcePath, IFormFile file)
+        {
+            var result = newPath(file);
+            if (sourcePath.Length > 0)
+            {
+                using (var stream = new FileStream(result, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            File.Delete(sourcePath);
+            return result;
+        }
+
+
+
+        public static string newPath(IFormFile file)
+        {
+            FileInfo fileInfo = new FileInfo(file.FileName);
+            string fileExtension = fileInfo.Extension;
+
+            string path = Environment.CurrentDirectory + @"\wwwroot\uploads";
+            var newPath = Guid.NewGuid().ToString() + "_" + DateTime.Now.Day + "_" + DateTime.Now.Month + "_" + DateTime.Now.Year + fileExtension;
+
+            string result = $@"{path}\{newPath}";
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ LinqHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
+
+
+namespace Helper
+{
+    public static class LinqHelper
+    {
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query, params Expression<Func<T, object>>[] includes) where T : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query, (IQueryable<T> current, Expression<Func<T, object>> include) => current.Include(include));
+            }
+
+            return query;
+        }
+
+        public static IOrderedQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> query, string propertyName)
+        {
+            Type typeFromHandle = typeof(TSource);
+            PropertyInfo property = typeFromHandle.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public);
+            if (property == null)
+            {
+                property = typeFromHandle.GetProperty("Id");
+            }
+
+            ParameterExpression parameterExpression = Expression.Parameter(typeFromHandle, "x");
+            MemberExpression body = Expression.Property(parameterExpression, property.Name);
+            LambdaExpression lambdaExpression = Expression.Lambda(body, parameterExpression);
+            Type typeFromHandle2 = typeof(Queryable);
+            MethodInfo methodInfo = (from m in typeFromHandle2.GetMethods()
+                                     where m.Name == "OrderBy" && m.IsGenericMethodDefinition
+                                     select m).Where(delegate (MethodInfo m)
+                                     {
+                                         List<ParameterInfo> list = m.GetParameters().ToList();
+                                         return list.Count == 2;
+                                     }).Single();
+            MethodInfo methodInfo2 = methodInfo.MakeGenericMethod(typeFromHandle, property.PropertyType);
+            return (IOrderedQueryable<TSource>)methodInfo2.Invoke(methodInfo2, new object[2] { query, lambdaExpression });
+        }
+
+        public static IOrderedQueryable<TSource> OrderByDesc<TSource>(this IQueryable<TSource> query, string propertyName)
+        {
+            Type typeFromHandle = typeof(TSource);
+            PropertyInfo property = typeFromHandle.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Instance | BindingFlags.Public);
+            if (property == null)
+            {
+                property = typeFromHandle.GetProperty("Id");
+            }
+
+            ParameterExpression parameterExpression = Expression.Parameter(typeFromHandle, "x");
+            MemberExpression body = Expression.Property(parameterExpression, property.Name);
+            LambdaExpression lambdaExpression = Expression.Lambda(body, parameterExpression);
+            Type typeFromHandle2 = typeof(Queryable);
+            MethodInfo methodInfo = (from m in typeFromHandle2.GetMethods()
+                                     where m.Name == "OrderByDescending" && m.IsGenericMethodDefinition
+                                     select m).Where(delegate (MethodInfo m)
+                                     {
+                                         List<ParameterInfo> list = m.GetParameters().ToList();
+                                         return list.Count == 2;
+                                     }).Single();
+            MethodInfo methodInfo2 = methodInfo.MakeGenericMethod(typeFromHandle, property.PropertyType);
+            return (IOrderedQueryable<TSource>)methodInfo2.Invoke(methodInfo2, new object[2] { query, lambdaExpression });
+        }
+    }
+}
+
+
+
+
+
+
+
+
